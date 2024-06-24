c3805afaf16612276083c2f5434a9bae81b922a9
Saber_Ben_Hamouda <benhamoudasabeur@gmail.com>
2024-06-24 22:13:08 +0200

    ⚡️improve app performance

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
 import { useState } from 'react';
+import './App.css';
 
 
 function App() {
-  const apiKey = "ecd160b9905d4ae007f35a7c0015e5e2"
-  const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q={city name}units=metric"
-  
-  async function checkWeather(){
-    const response = fetch(apiUrl + `&appid=${apiKey}`);
-    let data = await response.json();
-    return data ;
-  }
   const [city,setCity] = useState("");
+  const [weather,setWeather] = useState("weather");
+  const [humidity,setHymidity] = useState("");
+  const [temp,setTemp] = useState("");
+  const [wind,setWind] = useState("");
+
   const handleChange = (e) => {
     setCity(e.target.value);
   }
 
-  const [weather,setWeather] = useState("");
-  const [humidity,setHymidity] = useState("");
-  const [temp,setTemp] = useState("");
-  const [wind,setWind] = useState("");
+  const apiKey = "ecd160b9905d4ae007f35a7c0015e5e2"
+  const apiUrl =" https://api.openweathermap.org/data/2.5/weather?&units=metric"
+  
+  async function checkWeather(){
+
+    try {
+      const response = await  fetch(apiUrl +  `&q=${city}` +`&appid=${apiKey}` );
+      let data = await response.json();
+      
+      setHymidity(data.main.humidity);
+      setWeather(data.weather[0].main)
+      setTemp(data.main.temp);
+      setWind(data.wind.speed);
+      return data;
+    } catch(error)  {
+      console.error('Error:', error);
+  };
+    
+    }
+    
+
+  
+
+
 
 
   
@@ -28,11 +44,11 @@ function App() {
     <div>
       <div class="card">
       <div class="search">
-        <input type="text" onChange={handleChange} placeholder="enter city name " spellcheck="false"/>
+        <input type="text" onChange={handleChange} placeholder="enter city name " spellCheck="false"/>
         <button onClick={checkWeather}> <img src ="images/search.png "/> </button>
       </div>
       <div class="weather">
-        <img src={'images/{weather}.png'} class="weather-icon"/>
+        <img src={`images/${weather}.png`} class="weather-icon"/>
         <h1 className='temp'> {temp}°c</h1>
         <h2 className='city'> {city}</h2>
         <div className='details'>
@@ -44,7 +60,7 @@ function App() {
             </div>
           </div>
           <div className='col'>
-            <img src='wind.png'/>
+            <img src='images/wind.png'/>
             <div>
               <p className='wind'>{wind} kmh</p>
               <p>wind speed</p>
